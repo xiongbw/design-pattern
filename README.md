57ccdf3b380c1853f341b868ed20718493492d5a
xiongbw <beartheboss@163.com>
2023-10-28 20:14:59 +0800

    add: strategy-pattern module

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # 设计模式之 Java 最佳实践
+
+## 目录
+
+- [策略模式](./strategy-pattern)
---
+++ strategy-pattern/src/main/java/com/bowy/design/pattern/strategy/StrategyPatternApplication.java
@@ -0,0 +1,18 @@
+package com.bowy.design.pattern.strategy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 策略模式应用
+ *
+ * @author xiongbw
+ */
+@SpringBootApplication
+public class StrategyPatternApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StrategyPatternApplication.class, args);
+    }
+
+}
