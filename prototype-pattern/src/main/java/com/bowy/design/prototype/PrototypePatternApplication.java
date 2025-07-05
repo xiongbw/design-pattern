156870edf84caf8531382b0e7c1f331fe28e6ce3
xiongbw <beartheboss@163.com>
2025-07-05 14:22:23 +0800

    add: prototype-pattern module

---
+++ prototype-pattern/src/main/java/com/bowy/design/prototype/PrototypePatternApplication.java
@@ -0,0 +1,14 @@
+package com.bowy.design.prototype;
+
+/**
+ * 原型模式应用
+ *
+ * @author xiongbw
+ */
+public class PrototypePatternApplication {
+
+    public static void main(String[] args) {
+
+    }
+
+}
